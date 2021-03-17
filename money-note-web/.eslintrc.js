848be156312c41da8756b96d569f9df954691ff2
solidw <solidw@naver.com>
2021-03-17 23:50:05 +0900

    chore: Initialize with eslint --init

---
+++ money-note-web/.eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ['plugin:react/recommended', 'airbnb'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: ['react', '@typescript-eslint'],
+  rules: {
+    'react/react-in-jsx-scope': 'off',
+    'react/jsx-filename-extension': 'off',
+    'react/jsx-props-no-spreading': 'off',
+    'linebreak-style': [2, 'unix'],
+  },
+};
--- money-note-web/README.md
+++ money-note-web/README.md
@@ -1,7 +1,9 @@
 # Money Note Web
+
 The web service to provide Money Note
 
 ## Stack
+
 - React
 - Redux
 - Next.js
