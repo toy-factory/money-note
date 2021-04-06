87ee152e7d533a20f4293b1dabe9ac8844138edc
SuyeonChoi <tndus970120@gmail.com>
2021-04-07 06:39:28 +0900

    chore:Initialize server

--- money-note-server/src/main/java/com/note/money/MoneyNoteServerApplication.java
+++ money-note-server/src/main/java/com/note/money/MoneyNoteServerApplication.java
@@ -4,8 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Application {
-    public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
-    }
+public class MoneyNoteServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoneyNoteServerApplication.class, args);
+	}
+
 }
