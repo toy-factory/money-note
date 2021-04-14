b9087d81d83a4e1970b3662aea13a1494e04fe1c
SuyeonChoi <tndus970120@gmail.com>
2021-04-15 03:57:37 +0900

    feat: ExpenseByGroup 엔티티 구현

---
+++ money-note-server/src/main/java/com/note/money/domain/ExpenseByGroup.java
@@ -0,0 +1,27 @@
+package com.note.money.domain;
+
+import com.sun.istack.NotNull;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Getter
+@NoArgsConstructor
+@Entity
+public class ExpenseByGroup {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long expenseId;
+
+    private Long userId;
+
+    @NotNull
+    private Long groupId;
+
+    @NotNull
+    private int totalExpense;
+}
