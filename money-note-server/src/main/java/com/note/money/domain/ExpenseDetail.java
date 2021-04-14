cb38965f1fb8745d713cf14310e7c31deb706f7a
SuyeonChoi <tndus970120@gmail.com>
2021-04-15 03:59:53 +0900

    feat: ExpenseDetail 엔티티 구현

---
+++ money-note-server/src/main/java/com/note/money/domain/ExpenseDetail.java
@@ -0,0 +1,24 @@
+package com.note.money.domain;
+
+import com.sun.istack.NotNull;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Getter
+@NoArgsConstructor
+@Entity
+public class ExpenseDetail {
+    @Id
+    private Long expenseId;
+
+    @Id
+    private Long categoryId;
+
+    @NotNull
+    private int expense;
+}
