4c51c75da22f3d838a1b801eef061ace2422491e
SuyeonChoi <tndus970120@gmail.com>
2021-04-25 17:08:10 +0900

    refactor: Group 엔티티 이름 변경

--- money-note-server/src/main/java/com/note/money/MoneyNoteServerApplication.java
+++ money-note-server/src/main/java/com/note/money/MoneyNoteServerApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MoneyNoteServerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MoneyNoteServerApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(MoneyNoteServerApplication.class, args);
+    }
 
 }
