c43810e9bdd34096218ac6ed76ac1dcc9202dc74
SuyeonChoi <tndus970120@gmail.com>
2021-05-20 23:49:59 +0900

    test:그룹 생성

--- money-note-server/src/main/java/com/note/money/dto/CreateExpenseGroupRequest.java
+++ money-note-server/src/main/java/com/note/money/dto/CreateExpenseGroupRequest.java
@@ -1,11 +1,13 @@
 package com.note.money.dto;
 
 import com.note.money.domain.user.ExpenseGroup;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import org.hibernate.validator.constraints.Length;
 
 import javax.validation.constraints.NotBlank;
 
+@AllArgsConstructor
 @Getter
 public class CreateExpenseGroupRequest {
     @NotBlank
